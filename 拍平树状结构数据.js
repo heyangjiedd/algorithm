91826bbf0b99e6beba909becc813b3da7c54e994
YangJie He <1059320200@qq.com>
2021-01-18 16:57:39 +0800

    Update 拍平树状结构数据.js

--- 拍平树状结构数据.js
+++ 拍平树状结构数据.js
@@ -69,3 +69,39 @@ function flatten(arr) {
     return arr
 }
 console.log(flatten(arr))
+var ar = [
+  { id: 1, pid: null },
+  { id: 5, pid: 4 },
+  { id: 2, pid: 1 },
+  { id: 3, pid: 2 },
+  { id: 6, pid: 4 },
+  { id: 4, pid: 2 },
+  { id: 23, pid: 11 },
+  { id: 11, pid: null },
+  { id: 7, pid: 4 }
+];
+
+function arrToTree(arr) {
+  const map = {};
+  const root = [];
+  arr.forEach(item => {
+    if (item.pid === null) {
+      //根节点
+      map[item.id] = Object.assign(item, map[item.id]);
+      root.push(map[item.id]);
+    } else {
+      // 叶子节点
+      if (map[item.pid]) {
+        // map中存在上级节点，则添加到上级节点的children中
+        map[item.pid].children = map[item.pid].children || [];
+        map[item.pid].children.push(item);
+      } else {
+        // 不存在上级节点，则创造一个上级节点，ps:循环到上级节点时，直接替换。
+        map[item.pid] = { id: item.pid, pid: null, children: [item] };
+      }
+      map[item.id] = Object.assign(item, map[item.id]); // 设置当前id的map，若存在创造的节点，则合并
+    }
+  });
+  return root;
+}
+console.log(JSON.stringify(arrToTree(ar)));
